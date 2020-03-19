13b5094d1b1da781c920e3c5c6e24036734706b7
philipsenwout <philipsenwout@outlook.com>
2020-03-19 10:53:16 +0100

    02. Understanding Laravel facades

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Cache;
+
+class PagesController extends Controller
+{
+
+    public function home() 
+    {
+        Cache::remember('foo', 60, function() {
+            return 'foobar';
+        });
+
+        return Cache::get('foo');
+        // return File::get(public_path('index.php'));
+        // return Request::input('name'); 
+        // return View::make('welcome');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
+
+Route::get('/', 'PagesController@home');
 
 Route::get('/home', 'HomeController@index')
     ->name('home')
